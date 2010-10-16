d4e0e9f057e4f03ea05261fbccab143243cf7ff3
Dave Copeland <davetron5000@gmail.com>
2010-10-15 20:20:30 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carl::Application.config.secret_token = '0f8ec88feb1952899f32e8aec792116cc1b1fcc746a85f217dcc42fe6df45d0b9d10e3b0d306c0cb8afa7c7293a4d9b3eff9e0b318f40c72c8486db5354d0718'

3266f0d2441a6fcbad54a3cf08f8450310746173
Dave Copeland <davetron5000@gmail.com>
2010-10-15 23:30:10 -0400

    can edit solutions

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,2 +1,42 @@
 class SolutionsController < ApplicationController
+  before_filter :authenticate_player!
+  before_filter :check_access, :except => [:new,:create,:index]
+
+  def index
+    @solutions = Solution.find_all_by_player_id(current_player.id)
+  end
+
+  def create
+    code = params[:code].split(/\n/).map(&:strip)
+    solution = Solution.new(:player_id => current_player.id, :code => code)
+    solution.save
+    redirect_to edit_solution_url(solution)
+  end
+
+  def new
+    @solution = Solution.new(:player_id => current_player.id)
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+    code = params[:code].split(/\n/).map(&:strip)
+    @solution.code = code
+    @solution.save
+    redirect_to edit_solution_url(@solution)
+  end
+
+  private
+  def check_access
+    solution = Solution.find_by_id(params[:id])
+    if solution.player_id != current_player.id
+      head :forbidden
+    else
+      @solution = solution
+    end
+  end
 end
--- test/functional/solutions_controller_test.rb
+++ test/functional/solutions_controller_test.rb
@@ -1,8 +1,78 @@
 require 'test_helper'
 
 class SolutionsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  include Devise::TestHelpers
+
+  test "solutions requires login" do
+    get :index
+    assert_response 302
+  end
+
+  test "can see my solutions" do
+    sign_in players(:one)
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:solutions)
+    assert_equal 2,assigns(:solutions).size
+  end
+
+  test "can see one solution" do
+    sign_in players(:one)
+    get :show, :id => 1
+    assert_response :success
+    assert_not_nil assigns(:solution)
+  end
+
+  test "can get the new form for a new solution" do
+    sign_in players(:one)
+    get :new
+    assert_response :success
+    assert_not_nil assigns(:solution)
+    assert_equal assigns(:solution).player_id,players(:one).id
+  end
+
+  test "can create a new solution" do
+    player = players(:advanced_player)
+    sign_in player
+    post :create, :code => "move\nturnleft"
+    solutions = player.solutions
+    assert_equal 1,solutions.size
+    assert_equal ['move','turnleft'],solutions[0].code
+    assert_redirected_to :action => :edit, :id => solutions[0].id
+  end
+
+  test "can get the edit form" do
+    sign_in players(:one)
+    get :edit, :id => 1
+    assert_response :success
+    assert_not_nil assigns(:solution)
+  end
+
+  test "can edit an existing solution" do
+    player = players(:new_player)
+    sign_in player
+    put :update, :code => "move\nturnleft\nputbeacon\nputbeacon", :id => 3
+    solutions = player.solutions
+    assert_equal 1,solutions.size
+    assert_equal ['move','turnleft','putbeacon','putbeacon'],solutions[0].code
+    assert_redirected_to :action => :edit, :id => solutions[0].id
+  end
+
+  test "cannot see someone else's solution" do
+    sign_in players(:one)
+    get :show, :id => 3
+    assert_response :forbidden
+  end
+
+  test "cannot edit someone else's solution" do
+    sign_in players(:one)
+    get :edit, :id => 3
+    assert_response :forbidden
+  end
+
+  test "cannot update someone else's solution" do
+    sign_in players(:one)
+    put :update, :id => 3, :code => 'move'
+    assert_response :forbidden
   end
 end

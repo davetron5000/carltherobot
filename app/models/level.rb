b51777f2eb8db19d0359b79d58a4674635087b3a
Dave Copeland <davetron5000@gmail.com>
2010-10-16 12:50:30 -0400

    board object

--- app/models/level.rb
+++ app/models/level.rb
@@ -13,32 +13,76 @@ class Level < ActiveRecord::Base
   end
 end
 
+# A board is an 8x8 grid
 class Board
-  attr_accessor :rows
-  attr_accessor :columns
-  # Goal for this board
-  attr_accessor :goal
-  # 2x2 array of where the walls are
-  attr_accessor :map
-  # array of coordinates where beacons are
-  attr_accessor :beacons
-  # coordinate of where carl is
-  attr_accessor :carl
 
-  def self.fake_board
+  # Copies a board
+  def self.from_board(board)
     b = Board.new
-    b.rows = 3
-    b.columns = 3
-    b.goal = Goal.new
-    b.goal.carl = [0,2]
-    b.beacons = []
-    b.carl = [2,0]
-    b.map = [ 
-      [nil,nil,nil],
-      [nil,nil,nil],
-      [nil,nil,nil],
-    ]
+    b.map  = []
+    board.map.each do |x|
+      row = []
+      x.each { |y| row << y }
+      b.map << row
+    end
+    b.place_carl(*board.carl)
     b
+  end
+
+  attr_accessor :map
+
+  # Create an empty board
+  def initialize
+    @map = []
+    8.times do |x|
+      row = []
+      8.times { |y| row << nil }
+      @map << row
+    end
+  end
+
+  def beacon?(row,col)
+    return false if oob?(row,col)
+    @map[row][col] == :beacon
+  end
+
+  def wall?(row,col)
+    return true if oob?(row,col)
+    @map[row][col] == :wall
+  end
+
+  # Return carl's coordinates as [row,col]
+  def carl
+    @carl
+  end
+
+  def carl?(row,col)
+    return false if oob?(row,col)
+    @carl == [row,col]
+  end
+
+  def place_carl(row,col)
+    return false if oob?(row,col)
+    return false if wall?(row,col)
+    @carl = [row,col];
+    true
+  end
+
+  def remove_beacon(row,col)
+    return false if oob?(row,col)
+  end
+
+  def add_beacon(row,col)
+    return false if oob?(row,col)
+    return false if wall?(row,col)
+    @map[row][col] = :beacon
+    true
+  end
+
+  private 
+
+  def oob?(row,col)
+    row < 0 || row > 7 || col < 0 || col > 7
   end
 end
 
